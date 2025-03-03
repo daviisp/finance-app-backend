ae9957595cdfd4b8365ad811b16ce1b53bc94e52
Davi Silva <davi.pimenta8@gmail.com>
2025-03-03 14:19:56 -0300

    chore: disabeld rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly",
+      },
+    },
+    rules: {
+      "no-undef": ["error"],
+    },
+  },
   pluginJs.configs.recommended,
 ];
