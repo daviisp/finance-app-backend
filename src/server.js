a7083e23c08d63ba59a9a3bfde4bea00caf453c0
Davi Silva <davi.pimenta8@gmail.com>
2025-03-03 20:19:00 -0300

    feat: add post route to save user

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 import "dotenv/config.js";
 import express from "express";
+import cors from "cors";
+import { CreateUserController } from "./controllers/create-user.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 
+app.post("/api/user", (request, response) => {
+    const createUserController = new CreateUserController();
+    const { statusCode, body } = createUserController.execute(request);
+
+    return response.status(statusCode).json(body);
+});
+
 app.listen(PORT, () => {
-  console.log(`Backend started at port ${PORT}`);
+    console.log(`Backend started at port ${PORT}`);
 });
