65437355741dbd74b7b308ff3300cf23a8ed01e4
Davi Silva <davi.pimenta8@gmail.com>
2025-03-03 22:28:33 -0300

    feat: add route to get a user by id

--- src/server.js
+++ src/server.js
@@ -2,12 +2,20 @@ import "dotenv/config.js";
 import express from "express";
 import cors from "cors";
 import { CreateUserController } from "./controllers/create-user.js";
+import { GetUserByIdController } from "./controllers/get-user-by-id.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/users/:id", async (request, response) => {
+    const getUserByIdController = new GetUserByIdController();
+    const { statusCode, body } = await getUserByIdController.execute(request);
+
+    return response.status(statusCode).json(body);
+});
 
 app.post("/api/users", async (request, response) => {
     const createUserController = new CreateUserController();
