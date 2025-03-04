0ee2dfe480a19453fd8a43661e6039e1eea7ac8e
Davi Silva <davi.pimenta8@gmail.com>
2025-03-04 11:40:21 -0300

    feat: invocate get user by email repository to verify if user exists

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -1,8 +1,19 @@
 import bcrypt from "bcrypt";
 import { CreateUserRepository } from "../repositories/create-user.js";
+import { GetUserByEmailRepository } from "../repositories/get-user-by-email.js";
 
 export class CreateUserUseCase {
     async execute(createUserParams) {
+        const getUserByEmailRepository = new GetUserByEmailRepository();
+
+        const userExists = await getUserByEmailRepository.execute(
+            createUserParams.email
+        );
+
+        if (userExists) {
+            throw new Error("Email already in use");
+        }
+
         const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
 
         const createUserRepository = new CreateUserRepository();
