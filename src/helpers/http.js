63540e9be7a62bf5c476af5f9d989994004e7fef
Davi Silva <davi.pimenta8@gmail.com>
2025-03-04 21:19:05 -0300

    feat: create helper functions for HTTP responses

---
+++ src/helpers/http.js
@@ -0,0 +1,41 @@
+export const created = (body) => {
+    return {
+        statusCode: 201,
+        body,
+    };
+};
+
+export const updated = (body) => {
+    return {
+        statusCode: 200,
+        body,
+    };
+};
+
+export const found = (body) => {
+    return {
+        statusCode: 200,
+        body,
+    };
+};
+
+export const notFound = (body) => {
+    return {
+        statusCode: 404,
+        body,
+    };
+};
+
+export const badRequest = (body) => {
+    return {
+        statusCode: 400,
+        body,
+    };
+};
+
+export const internalServerError = (body) => {
+    return {
+        statusCode: 500,
+        body,
+    };
+};
