ceee8cfaa66770684d71002e34071c34a606a171
Davi Silva <davi.pimenta8@gmail.com>
2025-03-07 20:44:12 -0300

    refactor: add validation for user id in get user by id controller

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,14 +1,26 @@
 import { GetUserByIdUseCase } from "../use-cases/get-user-by-id.js";
-import { found, internalServerError, notFound } from "../helpers/http.js";
+import {
+    badRequest,
+    found,
+    internalServerError,
+    notFound,
+} from "../helpers/http.js";
+import validator from "validator";
 
 export class GetUserByIdController {
     async execute(httpRequest) {
         try {
+            const userId = httpRequest.params.id;
+
+            if (!userId || !validator.isUUID(userId)) {
+                return badRequest({
+                    errorMessage: "Missing or invalid id",
+                });
+            }
+
             const getUserByIdUseCase = new GetUserByIdUseCase();
 
-            const user = await getUserByIdUseCase.execute(
-                httpRequest.params.id
-            );
+            const user = await getUserByIdUseCase.execute(userId);
 
             if (!user) {
                 return notFound({
