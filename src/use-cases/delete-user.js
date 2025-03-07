5b6e052a1ff7dcedf2d64449b352846fd4f67e2c
Davi Silva <davi.pimenta8@gmail.com>
2025-03-07 20:46:31 -0300

    feat: create delete user use case

---
+++ src/use-cases/delete-user.js
@@ -0,0 +1,21 @@
+import { DeleteUserRepository } from "../repositories/delete-user";
+import { GetUserByIdRepository } from "../repositories/get-user-by-id";
+
+export class DeleteUserUseCase {
+    async execute(userId) {
+        const getUserByIdRepository = new GetUserByIdRepository();
+
+        const userExists = await getUserByIdRepository.execute(userId);
+        if (!userExists) {
+            return {
+                errorMessage: "User not found",
+            };
+        }
+
+        const deleteUserRepository = new DeleteUserRepository();
+
+        const deletedUser = await deleteUserRepository.execute(userId);
+
+        return deletedUser;
+    }
+}
