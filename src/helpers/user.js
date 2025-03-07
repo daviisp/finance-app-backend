592e2aa5c4c58dc81a31e0c59c0c06c96d782837
Davi Silva <davi.pimenta8@gmail.com>
2025-03-07 20:44:59 -0300

    feat: add uuid validation helper

--- src/helpers/user.js
+++ src/helpers/user.js
@@ -1,11 +1,11 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 
+export const verifyIfIdIsUUID = (id) => validator.isUUID(id);
+
 export const verifyIfPasswordIsValid = (password) =>
     password.trim().length >= 6;
 
 export const verifyIfEmailIsValid = (email) => validator.isEmail(email);
 
-export const hashPassword = async (password) => {
-    return bcrypt.hash(password, 10);
-};
+export const hashPassword = async (password) => await bcrypt.hash(password, 10);
