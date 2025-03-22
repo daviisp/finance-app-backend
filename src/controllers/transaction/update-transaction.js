bbeb75d0bc7ccfec872b77e0875223b96640f383
Davi Silva <davi.pimenta8@gmail.com>
2025-03-22 17:54:01 -0300

    feat: create update transaction controller

---
+++ src/controllers/transaction/update-transaction.js
@@ -0,0 +1,83 @@
+import {
+    badRequest,
+    internalServerError,
+    updated,
+} from "../../helpers/http.js";
+import { verifyIfIdIsUUID } from "../../helpers/user.js";
+import validator from "validator";
+
+export class UpdateTransactionControlller {
+    constructor(updateTransactionUseCase) {
+        this.updateTransactionUseCase = updateTransactionUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const transactionId = httpRequest.params.id;
+            if (!transactionId || !verifyIfIdIsUUID(transactionId)) {
+                return badRequest({
+                    errorMessage: "Missing or invalid id",
+                });
+            }
+
+            const params = httpRequest.body;
+
+            if (
+                (!params.name || !params.name.trim()) &&
+                (!params.type || !params.type.trim()) &&
+                (!params.amount || !params.amount.trim()) &&
+                (!params.date || !params.date.trim())
+            ) {
+                return badRequest({
+                    errorMessage: "At least one field must be provided",
+                });
+            }
+
+            if (params.type) {
+                const typeValid = ["EARNING", "EXPENSE", "INVESTMENT"].includes(
+                    params.type.toUpperCase()
+                );
+
+                if (!typeValid) {
+                    return badRequest({
+                        errorMessage: "Invalid type",
+                    });
+                }
+            }
+
+            if (params.amount !== undefined && params.amount !== null) {
+                if (params.amount === 0) {
+                    return badRequest({
+                        errorMessage: "Amount must be greater than zero.",
+                    });
+                }
+                const amountToString = params.amount.toFixed(2).toString();
+                const amountIsValid = validator.isCurrency(amountToString, {
+                    allow_decimal: true,
+                    decimal_separator: ".",
+                    digits_after_decimal: [2],
+                });
+
+                if (!amountIsValid) {
+                    return badRequest({
+                        errorMessage: "Invalid amount",
+                    });
+                }
+            }
+
+            const result = await this.updateTransactionUseCase.execute(
+                transactionId,
+                params
+            );
+
+            if (result.errorMessage) {
+                return badRequest({
+                    errorMessage: result.errorMessage,
+                });
+            }
+            return updated(result);
+        } catch (err) {
+            console.error(err);
+            return internalServerError();
+        }
+    }
+}
