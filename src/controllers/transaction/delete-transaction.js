9eca6d5fbdd79498cc01c3e7253e75ad4e08e351
Davi Silva <davi.pimenta8@gmail.com>
2025-03-23 16:44:56 -0300

    feat: create delete transaction controller

---
+++ src/controllers/transaction/delete-transaction.js
@@ -0,0 +1,30 @@
+import {
+    badRequest,
+    internalServerError,
+    okEmpty,
+} from "../../helpers/http.js";
+import validator from "validator";
+
+export class DeleteTransactionController {
+    constructor(deleteTransactionUseCase) {
+        this.deleteTransactionUseCase = deleteTransactionUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const transactionId = httpRequest.params.id;
+
+            if (!transactionId || !validator.isUUID(transactionId)) {
+                return badRequest({
+                    errorMessage: "Missing or invalid id",
+                });
+            }
+
+            await this.deleteTransactionUseCase.execute(transactionId);
+
+            return okEmpty();
+        } catch (err) {
+            console.error(err);
+            return internalServerError();
+        }
+    }
+}
