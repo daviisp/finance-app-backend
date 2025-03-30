e6c854221f85888d3d82bdd252b4cc0e248e62ec
Davi Silva <davi.pimenta8@gmail.com>
2025-03-29 23:50:13 -0300

    fix: separate createOrUpdateUserSchema into distinct schemas

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,8 +1,26 @@
 import { z } from "zod";
 
-export const createOrUpdateUserSchema = z.object({
+export const createUserSchema = z.object({
     firstName: z.string().trim().min(1),
     lastName: z.string().trim().min(1),
     email: z.string().email(),
     password: z.string().trim().min(6),
 });
+
+export const updateUserSchema = z
+    .object({
+        firstName: z.string().trim().min(1).optional(),
+        lastName: z.string().trim().min(1).optional(),
+        email: z.string().email().optional(),
+        password: z.string().trim().min(6).optional(),
+    })
+    .refine(
+        (data) => {
+            return (
+                data.firstName || data.lastName || data.email || data.password
+            );
+        },
+        {
+            message: "At least one field must be provided",
+        }
+    );
