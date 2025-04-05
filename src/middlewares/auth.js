bb5e76945d64445df3a4cd76aebed8b5d72286da
Davi Silva <davi.pimenta8@gmail.com>
2025-04-05 15:27:35 -0300

    feat: create middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+export const auth = (request, response, next) => {
+    try {
+        const accessToken = request.headers?.authorization?.split("Bearer ")[1];
+
+        if (!accessToken) {
+            return response.status(401).json({ errorMessage: "Unauthorized." });
+        }
+
+        const decodedToken = jwt.verify(
+            accessToken,
+            process.env.JWT_ACCESS_TOKEN_SECRET
+        );
+
+        if (!decodedToken) {
+            return response.status(401).json({ errorMessage: "Unauthorized." });
+        }
+
+        request.userId = decodedToken.userId;
+        next();
+    } catch (error) {
+        console.error(error);
+        return response.status(401).json({ errorMessage: "Unauthorized." });
+    }
+};
