798012791407526ffe0e6ce2192e2bcac0fd8727
Davi Silva <davi.pimenta8@gmail.com>
2025-04-05 15:55:58 -0300

    feat: add error messages to schemas

--- src/schemas/transaction.js
+++ src/schemas/transaction.js
@@ -3,35 +3,59 @@ import validator from "validator";
 
 export const createTransactionSchema = z.object({
     userId: z.string().uuid(),
-    name: z.string().trim().min(1),
-    date: z.coerce.date(),
-    type: z.enum(["EARNING", "EXPENSE", "INVESTMENT"]),
+    name: z.string().trim().min(1, {
+        message: "Transaction name is required.",
+    }),
+    date: z.coerce.date({
+        message: "Invalid date format.",
+    }),
+    type: z.enum(["EARNING", "EXPENSE", "INVESTMENT"], {
+        message: "Type must be 'EARNING', 'EXPENSE', or 'INVESTMENT'.",
+    }),
     amount: z
         .number()
-        .positive()
-        .refine((value) => {
-            return validator.isCurrency(value.toFixed(2), {
-                allow_decimal: true,
-                decimal_separator: ".",
-                digits_after_decimal: [2],
-            });
-        }),
+        .positive({
+            message: "Amount must be greater than 0.",
+        })
+        .refine(
+            (value) => {
+                return validator.isCurrency(value.toFixed(2), {
+                    allow_decimal: true,
+                    decimal_separator: ".",
+                    digits_after_decimal: [2],
+                });
+            },
+            { message: "Invalid currency format." }
+        ),
 });
 
-export const updateTransactionSchema = z.object({
-    transactionId: z.string().uuid(),
-    name: z.string().trim().min(1).optional(),
-    date: z.coerce.date().optional(),
-    type: z.enum(["EARNING", "EXPENSE", "INVESTMENT"]).optional(),
-    amount: z
-        .number()
-        .positive()
-        .refine((value) => {
-            return validator.isCurrency(value.toFixed(2), {
-                allow_decimal: true,
-                decimal_separator: ".",
-                digits_after_decimal: [2],
-            });
-        })
-        .optional(),
-});
+export const updateTransactionSchema = z
+    .object({
+        transactionId: z.string().uuid(),
+        name: z.string().trim().min(1).optional(),
+        date: z.coerce.date().optional(),
+        type: z.enum(["EARNING", "EXPENSE", "INVESTMENT"]).optional(),
+        amount: z
+            .number()
+            .positive()
+            .refine(
+                (value) => {
+                    return validator.isCurrency(value.toFixed(2), {
+                        allow_decimal: true,
+                        decimal_separator: ".",
+                        digits_after_decimal: [2],
+                    });
+                },
+                {
+                    message: "Invalid currency format.",
+                }
+            )
+            .optional(),
+    })
+    .refine(
+        (data) =>
+            data.name || data.date || data.type || data.amount !== undefined,
+        {
+            message: "At least one field must be provided.",
+        }
+    );
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,10 +1,18 @@
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-    firstName: z.string().trim().min(1),
-    lastName: z.string().trim().min(1),
-    email: z.string().email(),
-    password: z.string().trim().min(6),
+    firstName: z.string().trim().min(1, {
+        message: "First name is required.",
+    }),
+    lastName: z.string().trim().min(1, {
+        message: "Last name is required.",
+    }),
+    email: z.string().email({
+        message: "Email is required.",
+    }),
+    password: z.string().trim().min(6, {
+        message: "Password must be at least 6 characters.",
+    }),
 });
 
 export const updateUserSchema = z
@@ -26,6 +34,10 @@ export const updateUserSchema = z
     );
 
 export const loginUserSchema = z.object({
-    email: z.string().email(),
-    password: z.string().trim().min(6),
+    email: z.string().email({
+        message: "Email is required.",
+    }),
+    password: z.string().trim().min(6, {
+        message: "Password must be at least 6 characters.",
+    }),
 });
